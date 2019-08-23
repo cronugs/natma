ba5ef2c1d9c2214baa577440fd8bb1d07b859c87
cronugs <cronugs@outlook.com>
2019-08-23 23:13:49 +0200

    added routes and templates for adding and editing forms. Added static/css/style.css

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+#MONGO_URI = os.environ.get('MONGO_URI')
+
+app.config["MONGO_URI"] = os.environ.get('MONGO_NATMA_URI')
+app.config["MONGO_DBNAME"] = 'task_manager'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html",
+                           tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_categories)
+
 
 
 if __name__ == '__main__':
